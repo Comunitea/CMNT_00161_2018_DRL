09ef68393933b9901502b24b0075874e10a3428e
JesusVMayor <jesus@comunitea.com>
2019-12-24 12:07:30 +0100

    [IMP]milk_planning: Se tienen en cuenta las producciones.

--- project_addons/milk_planning/models/res_config_settings.py
+++ project_addons/milk_planning/models/res_config_settings.py
@@ -5,46 +5,50 @@ from odoo import api, fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
-    product_raw_milk = fields.Many2one('product.product')
-    product_raw_milk_do = fields.Many2one('product.product')
-    product_raw_milk_100 = fields.Many2one('product.product')
-    product_skimmed_milk = fields.Many2one('product.product')
-    product_cream = fields.Many2one('product.product')
+    product_raw_milk = fields.Many2one("product.product")
+    product_raw_milk_do = fields.Many2one("product.product")
+    product_raw_milk_100 = fields.Many2one("product.product")
+    product_skimmed_milk = fields.Many2one("product.product")
+    product_cream = fields.Many2one("product.product")
 
     @api.model
     def get_values(self):
         res = super().get_values()
-        get_param = self.env['ir.config_parameter'].sudo().get_param
+        get_param = self.env["ir.config_parameter"].sudo().get_param
         res.update(
-            product_raw_milk=int(get_param(
-                'milk_planning.product_raw_milk', default=False
-            )),
-            product_raw_milk_do=int(get_param(
-                'milk_planning.product_raw_milk_do', default=False
-            )),
-            product_raw_milk_100=int(get_param(
-                'milk_planning.product_raw_milk_100', default=False
-            )),
-            product_skimmed_milk=int(get_param(
-                'milk_planning.product_skimmed_milk', default=False
-            )),
-            product_cream=int(get_param(
-                'milk_planning.product_cream', default=False
-            )),
+            product_raw_milk=int(
+                get_param("milk_planning.product_raw_milk", default=False)
+            ),
+            product_raw_milk_do=int(
+                get_param("milk_planning.product_raw_milk_do", default=False)
+            ),
+            product_raw_milk_100=int(
+                get_param("milk_planning.product_raw_milk_100", default=False)
+            ),
+            product_skimmed_milk=int(
+                get_param("milk_planning.product_skimmed_milk", default=False)
+            ),
+            product_cream=int(
+                get_param("milk_planning.product_cream", default=False)
+            ),
         )
 
         return res
 
     def set_values(self):
         super().set_values()
-        set_param = self.env['ir.config_parameter'].sudo().set_param
+        set_param = self.env["ir.config_parameter"].sudo().set_param
 
-        set_param('milk_planning.product_raw_milk', self.product_raw_milk.id)
-        set_param('milk_planning.product_raw_milk_do',
-                  self.product_raw_milk_do.id)
-        set_param('milk_planning.product_raw_milk_100',
-                  self.product_raw_milk_100.id)
-        set_param('milk_planning.product_skimmed_milk', self.product_skimmed_milk.id)
-        set_param('milk_planning.product_cream', self.product_cream.id)
+        set_param("milk_planning.product_raw_milk", self.product_raw_milk.id)
+        set_param(
+            "milk_planning.product_raw_milk_do", self.product_raw_milk_do.id
+        )
+        set_param(
+            "milk_planning.product_raw_milk_100", self.product_raw_milk_100.id
+        )
+        set_param(
+            "milk_planning.product_skimmed_milk", self.product_skimmed_milk.id
+        )
+        set_param("milk_planning.product_cream", self.product_cream.id)
