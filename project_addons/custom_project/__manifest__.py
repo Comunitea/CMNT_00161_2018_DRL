55379de89ea366e4455d2420d9bcdeb602225f54
Santi <santi@comunitea.com>
2020-12-13 15:34:39 +0100

    [ADD] custom_project

---
+++ project_addons/custom_project/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2020 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Project customizations',
+    'version': '12.0.1.0.0',
+    'summary': '',
+    'category': 'Uncategorized',
+    'author': 'Comunitea',
+    'license': 'AGPL-3',
+    'depends': [
+        'project_parent_task_filter',
+    ],
+    'data': [
+        'views/project_views.xml',
+    'installable': True,
+}
