c39a8d61b5d1f7c6acf116c4c88dee41c4cd6598
Kiko Sánchez <kiko@comunitea.com>
2020-12-21 18:31:56 +0100

    [FIX] quality_control_multi_test

--- project_addons/quality_control_multi_test/models/stock_picking.py
+++ project_addons/quality_control_multi_test/models/stock_picking.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Comunitea
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 from odoo.exceptions import UserError, ValidationError
 
 
@@ -14,7 +14,7 @@ class StockPicking(models.Model):
     def action_done(self):
         res = super().action_done()
         
-        valid = all([x.valid_for_move for x in qc_inspections_ids])
+        valid = all([x.valid_for_move for x in self.qc_inspections_ids])
         
         if not valid:
             raise ValidationError(_('Before validate th picking you need to fill at least on inspecion . You can need to geneate tests'))
