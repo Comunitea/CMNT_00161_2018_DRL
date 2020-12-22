52b54cef584307da15dfc1b48d4cdc94970d6b81
Kiko Sánchez <kiko@comunitea.com>
2020-12-22 13:32:38 +0100

    [DEV] purchase_custom

---
+++ project_addons/purchase_custom/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2019 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Purchase Custom',
+    'version': '12.0.1.0.0',
+    'category': 'Sales',
+    'author': 'Comunitea',
+    'maintainer': 'Comunitea',
+    'website': 'www.comunitea.com',
+    'license': 'AGPL-3',
+    'depends': ['purchase_stock',
+                ],
+    'data': [
+    ],
+    'installable': True,
+}
---
+++ project_addons/purchase_custom/models/purchase_order.py
@@ -0,0 +1,34 @@
+# © 2019 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import api, fields, models, _
+import logging
+_logger = logging.getLogger(__name__)
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    @api.multi
+    def _create_picking(self):
+        super()._create_picking()
+        StockPicking = self.env['stock.picking']
+        for order in self:
+            pickings = order.picking_ids.filtered(lambda x: x.state not in ('done', 'cancel'))
+            for pick in pickings:
+                moves = pick.move_lines.filtered(lambda x: x.purchase_line_id.date_planned != pick.scheduled_date)
+                while moves:
+                    _logger.info("Se separa el movimienro %s"%moves[0].display_name)
+                    scheduled_date = moves[0].purchase_line_id.date_planned
+                    res = order._prepare_picking()
+                    res['scheduled_date'] = scheduled_date
+                    new_picking = StockPicking.create(res)
+                    _logger.info("Se crea el nuevo albarán%s" % new_picking.display_name)
+                    moves_to_assign = moves.filtered(lambda x: x.purchase_line_id.date_planned == scheduled_date)
+                    for move in moves_to_assign:
+                        move.picking_id = new_picking
+                    moves -= moves_to_assign
+                    new_picking.message_post_with_view('mail.message_origin_link',
+                                                   values={'self': new_picking, 'origin': order},
+                                                   subtype_id=self.env.ref('mail.mt_note').id)
+        return True
+
+
